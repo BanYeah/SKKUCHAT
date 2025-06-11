21d0d5db82e16d6d903e6de861f192eb5a9b6101
BanYeah <04smailing@gmail.com>
2025-06-12 04:12:41 +0900

    Fix: Query Generation

--- run.py
+++ run.py
@@ -1,59 +1,41 @@
-import crawling, query_generation, bm25, response_generation, build_dataset
+import argparse
 
-import numpy as np
-from sklearn.metrics import accuracy_score
-import argparse, sys
+from utils.Crawler import Crawler
+from utils.QueryGen import QueryGen
+# from utils.ResponseGen import ResponseGen
+# from utils.build_dataset import build_dataset
 
-# 데이터 크롤링
-# 쿼리 생성
-def prepare_bm25():
-    crawling.crawl_skku_notice()
-    query_generation.generate_query(crawling.get_data())
+def main(args):
+    if args.mode == "prepare":
+        Crawler(max_pages=10, save2DB=False)
+        QueryGen()
 
-# hit rate 계산
-def eval_bm25(k):
-    data = crawling.get_data()
-    query = query_generation.get_query()
-    model = bm25.BM25(data, query)
+    if args.model == "bm25":
+        if args.mode == "train":
+            print("This model does not require training.")
+        # elif args.mode == "eval":
+        #     eval_bm25(args.k) # hitrate
+    elif args.model == "rag":
+        if args.mode == "prepare":
+            pass
+            # ResponseGen.generate_response(notices, queries)
+            # build_dataset.build_dataset(notices, ResponseGen.get_response())
+        elif args.mode == "train" or args.mode == "eval":
+            print("Training/Evaluation is not supported yet. This feature will be added in a future version.")
 
-    hit_rate = model.get_hitrate(k)
-
-    print(f"hit rate: {hit_rate}")
-
-def prepare_rag():
-    prepare_bm25()
-    response_generation.generate_response(crawling.get_data(), query_generation.get_query())
-    build_dataset.build_dataset(crawling.get_data(), response_generation.get_response())
-
-def main(args):   
-    if args.model == 'bm25':
-        if args.mode == 'prepare':              # prepare
-            prepare_bm25()
-        elif args.mode == 'eval':             # bm25 eval
-            eval_bm25(args.k)
-        elif args.mode == 'conversation':   # bm25 demo
-            # demo
-            exit(1)
-        else:
-            print('invalid argument')
-            exit(1)
-    elif args.model == 'rag':
-        if args.mode == 'prepare':
-            prepare_rag()
 
 # 사용법
-# python run.py -mode MODE -model MODEL -k K
 # python run.py -mode=MODE -model=MODEL -k=K
-if __name__ == '__main__' :
+if __name__ == "__main__" :
     parser = argparse.ArgumentParser()
-    parser.add_argument('-mode', help=' : running mode (prepare, train, eval, conversation)') 
-    parser.add_argument('-model', help=' : select model (bm25, rag)')
-    parser.add_argument('-k', type=int, help=' : top-k in evaluation', default=3) 
+    parser.add_argument("-mode", help=" : select running mode (prepare, train, eval)") 
+    parser.add_argument("-model", help=" : select model (bm25, rag)")
+    parser.add_argument("-k", type=int, help=" : top-k in evaluation", default=3) 
 
     # argument valid check
     args = parser.parse_args()
-    if args.mode not in ['prepare', 'train', 'eval', 'conversation']:           
-        print('invalid argument')
+    if args.mode not in ["prepare", "train", "eval"] or args.model not in ["bm25", "rag"]:
+        print("Invalid argument.")
         exit(1)
 
     main(args)
--- utils/Crawler.py
+++ utils/Crawler.py
@@ -192,5 +192,5 @@ def Crawler(max_pages: int, img_OCR: bool = True, save2DB: bool = True):
         conn.close()
     else:
         # 크롤링한 데이터를 JSON으로 저장
-        with open("skku_notices.json", "w", encoding="utf-8") as f:
+        with open("./data/skku_notices.json", "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)

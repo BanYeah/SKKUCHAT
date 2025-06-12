ace2a6dd4dc584ff7eacb33303fa442121c55184
BanYeah <04smailing@gmail.com>
2025-06-12 11:31:38 +0900

    Docs:

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # SKKUCHAT
-> 챗봇 기반의 학교 공지사항 정보 제공 서비스
+**챗봇 기반의 학교 공지사항 정보 제공 서비스**
 <br>
 
-기존 챗봇 기반 학교 정보 제공 서비스는 규칙 기반으로 최신 정보 반영에 한계가 있어, RAG 기술을 활용한 고도화된 챗봇 개발을 목표로 설정하였습니다.
+기존 챗봇 기반 학교 정보 제공 서비스는 규칙 기반으로 최신 정보 반영에 한계가 있어, RAG 기술을 활용한 고도화된 챗봇 개발을 목표로 하고 있습니다. 다양한 출처에 흩어진 정보를 통합하기엔 초기 단계에서 무리가 있어, 1차적으로 학교 공지사항을 중심으로 정보를 제공하는 MVP 형태로 설계 및 구현하였습니다.
+<br>
 <br>
 <br>
 
 ## Key Features
-* 학교 공지사항 정보 웹 크롤링 및 데이터 정제
+* 최신 학교 공지사항 정보 웹 크롤링 및 데이터 정제 자동화
 * BM25 검색 엔진 기반 공지사항 검색
   * Reranking 및 유사도 임계값 설정으로 검색 결과 정제
 * ChatGPT 기반 공지사항 정보 요약 및 질문 응답
@@ -21,20 +22,26 @@
 <br>
 
 ## Quick Start Guide
-의존성 설치
+Step 1. 의존성 설치
 ```bash
+# OCR용 패키지 설치 및 Mecab 설치
 chmod +x setup.sh
 ./setup.sh
 
-pip install -r requirements.txt # Mecab 설치 확인
+# 파이썬 모듈 설치
+pip install -r requirements.txt
 ```
 <br>
 
-웹 페이지 실행
+Step 2. ```.env```파일에 OPENAI_API_KEY 작성
 ```bash
-streamlit run streamlit.py
+OPENAI_API_KEY = ''
 ```
-<br>
+
+Step 3. 웹 페이지 실행
+```bash
+streamlit run app.py
+```
 <br>
 
 > @SKKAI (Sungkyunkwan University AI Association)    
--- setup.sh
+++ setup.sh
@@ -6,23 +6,33 @@ pip install pytesseract
 sudo apt install tesseract-ocr
 sudo apt install tesseract-ocr-kor
 
-# Mecab-ko 설치 (Ubuntu)
-# wget https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz
-# tar xvfz mecab-0.996-ko-0.9.2.tar.gz
-# cd mecab-0.996-ko-0.9.2
-# ./configure
-# make
-# make check
-# sudo make install
+# Mecab-ko 설치 여부 확인
+echo
+read -p "Do you want to install Mecab? (y/n): " install_mecab
 
-# Mecab-dic 설치 (Ubuntu)
-# wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz
-# tar xvfz mecab-ko-dic-2.1.1-20180720.tar.gz
-# cd mecab-ko-dic-2.1.1-20180720
-# ./configure
-# make
-# sudo make install
+if [ "$install_mecab" == "y" ] || [ "$install_mecab" == "Y" ]; then
+    # Mecab-ko 설치 (Ubuntu)
+    cd ~
+    wget https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz
+    tar xvfz mecab-0.996-ko-0.9.2.tar.gz
+    cd mecab-0.996-ko-0.9.2
+    ./configure
+    make
+    make check
+    sudo make install
 
-# Mecab-dic이 존재하지 않는다는 에러 발생 시
-# sudo mkdir -p /usr/local/lib/mecab/dic
-# sudo ln -s /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ko-dic /usr/local/lib/mecab/dic/mecab-ko-dic
+    # Mecab-dic 설치 (Ubuntu)
+    cd ~
+    wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz
+    tar xvfz mecab-ko-dic-2.1.1-20180720.tar.gz
+    cd mecab-ko-dic-2.1.1-20180720
+    ./configure
+    make
+    sudo make install
+
+    # Mecab-dic 경로 문제 해결
+    sudo mkdir -p /usr/local/lib/mecab/dic
+    sudo ln -s /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ko-dic /usr/local/lib/mecab/dic/mecab-ko-dic
+else
+    echo "Skipping Mecab installation."
+fi
